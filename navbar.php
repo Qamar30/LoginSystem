e23b3ba0546b9135e777af54ba529dadb3b97508
qamar <qamarkhoza@gmail.com>
2019-05-09 08:53:53 +0200

    updated redirect link

--- navbar.php
+++ navbar.php
@@ -18,7 +18,7 @@
  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
    <ul class="navbar-nav mr-auto">
      <li class="nav-item active">
-       <a class="nav-link" href="register.php">Home <span class="sr-only">(current)</span></a>
+       <a class="nav-link" href="welcome.php">Home <span class="sr-only">(current)</span></a>
      </li>
 
 
