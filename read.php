fbf43d0c10866e040b7f71b98334f2bc4b561ebd
qamar <qamarkhoza@gmail.com>
2019-05-09 02:18:36 +0200

    final draft before final version

---
+++ read.php
@@ -0,0 +1,84 @@
+<?php
+    include 'navbar.php';
+ ?>
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    
+    <title>Dashboard</title>
+
+    <style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 100;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Account Details</h2>
+                        <a href="register.php" class="btn btn-success pull-right">Add New Account</a>
+                        <a href="welcome.php" class="btn btn-success pull-right">Home</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM users";
+                    if($result = $mysqli->query($sql)){
+                        if($result->num_rows > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>username</th>";
+                                        echo "<th>password</th>";
+
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = $result->fetch_array()){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['username'] . "</td>";
+                                        echo "<td>" . $row['password'] . "</td>";
+
+                                }
+                                echo "</tbody>";
+                            echo "</table>";
+                            // Free result set
+                            $result->free();
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+                    }
+
+                    // Close connection
+                    $mysqli->close();
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
